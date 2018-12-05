0d6ced0dbe212e60961c13c7d59e7e48220d89fe
dima <dima@pizza.com>
2018-12-05 17:17:58 +0400

    add contact divider

--- app/src/main/java/com/testing/android/countach/listing/ContactListFragment.java
+++ app/src/main/java/com/testing/android/countach/listing/ContactListFragment.java
@@ -3,6 +3,9 @@ package com.testing.android.countach.listing;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -63,6 +66,7 @@ final public class ContactListFragment extends MvpAppCompatFragment implements C
         super.onViewCreated(view, savedInstanceState);
         RecyclerView recyclerContactList = view.findViewById(R.id.recycler_view_contact_list);
         recyclerContactList.setLayoutManager(new LinearLayoutManager(requireContext()));
+        recyclerContactList.addItemDecoration(new ContactDecoration(requireContext()));
         contactAdapter = new ContactListAdapter(listener);
         recyclerContactList.setAdapter(contactAdapter);
     }
@@ -110,4 +114,40 @@ final public class ContactListFragment extends MvpAppCompatFragment implements C
         contactAdapter.submitList(list);
     }
 
+    private static class ContactDecoration extends RecyclerView.ItemDecoration {
+        private Drawable divider;
+        private final Rect bounds = new Rect();
+
+        ContactDecoration(Context context) {
+            this.divider = ContextCompat.getDrawable(context, R.drawable.divider_contact);
+        }
+
+        @Override
+        public void onDraw(@NonNull Canvas canvas, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+            canvas.save();
+            int left;
+            int right;
+            if (parent.getClipToPadding()) {
+                left = parent.getPaddingLeft();
+                right = parent.getWidth() - parent.getPaddingRight();
+                canvas.clipRect(left, parent.getPaddingTop(), right, parent.getHeight() - parent.getPaddingBottom());
+            } else {
+                left = 0;
+                right = parent.getWidth();
+            }
+
+            int childCount = parent.getChildCount();
+
+            for (int i = 0; i < childCount; ++i) {
+                View child = parent.getChildAt(i);
+                parent.getDecoratedBoundsWithMargins(child, this.bounds);
+                int bottom = this.bounds.bottom + Math.round(child.getTranslationY());
+                int top = bottom - this.divider.getIntrinsicHeight();
+                this.divider.setBounds(left, top, right, bottom);
+                this.divider.draw(canvas);
+            }
+
+            canvas.restore();
+        }
+    }
 }
